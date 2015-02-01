c3acfd8420255d9a7817669097d264c22aa6a720
root <root@localhost.localdomain>
2015-01-31 16:10:51 -0800

    modified:   code/jishi.user.js

--- code/jishi.user.js
+++ code/jishi.user.js
@@ -68,7 +68,7 @@ function procRole(id,item){
   }
   );
 }
-function old_procRole(id,item){
+function old_funcprocrole(id,item){
   //////var tgskills = ["心佛","五郎","古谱","九宫","鸳鸯","虬枝","金蛇","玲珑" ]
   var skills="";
   var resp="";
@@ -95,7 +95,66 @@ function addskilltext(item,str){
   item.appendChild(elBtdiv);
 }
 
+function matchrolelist(txt){
+  if(localStorage.okrolelist.match(txt)){
+    return "okrolename";
+  }
+  if(localStorage.ngrolelist.match(txt)){
+    return "ngrolename";
+  }
+  return "unrolename";
+}
+function updatelist(elm,txt,rolelist,okng){
+  if(rolelist.match(txt)){
+    rolelist=rolelist.replace(eval("/"+txt+"/g"),"")
+  }else{
+    rolelist+=txt
+  }
+  
+  if(okng){
+    localStorage.okrolelist = rolelist;
+  }else{
+    localStorage.ngrolelist = rolelist;
+  }
+  elm.id=matchrolelist(txt)
+  console.log(localStorage.ngrolelist+" NG");
+  console.log(localStorage.okrolelist+" OK");
+}
+
+
+function markrole(){
+  var titdivs = document.getElementsByClassName('tit')
+  for(i=0;i<titdivs.length;i++){
+    marktitdiv(titdivs[i],getrolename(titdivs[i]));
+  }
+
+}
+function getrolename(elm){
+  if(elm.getAttribute('rolename')){
+    return elm.getAttribute('rolename')
+  }
+  return elm.innerHTML
+}
+
+function marktitdiv(elm,txt){
+  var elmdiv = document.createElement('div')
+  elm.setAttribute("rolename",txt)
+  //elm.className=matchrolelist(txt)
+  elm.id=matchrolelist(txt)
+  
+  elmdiv.innerHTML+=txt+"<a id='mark_ngp' class='mark_ng' href=\"javascript:void();\">[  NG  ]</a>" +
+           "<a id='mark_ok' class='mark_ok' href=\"javascript:void();\">[  OK  ]</a>"
+  
+  elm.innerHTML=""
+  elm.appendChild(elmdiv)
+  elm.getElementsByClassName('mark_ng')[0].addEventListener('click',function(){updatelist(elm,txt,localStorage.ngrolelist,false);},false)
+  elm.getElementsByClassName('mark_ok')[0].addEventListener('click',function(){updatelist(elm,txt,localStorage.okrolelist,true);},false)
+   
+}
+
+
 function runview(){
+  markrole();
   tb=document.getElementById("findSellingGoods")
   tblist=document.getElementById("findNoticeGoods")
   serverName=getCookie("serverName")
@@ -109,7 +168,7 @@ function runview(){
   tbli=tb.getElementsByTagName("li")
   for(i=0;i<tbli.length;i++){
     try{
-      a=tbli[i].getElementsByTagName("a")[0]
+      a=tbli[i].getElementsByClassName("pic")[0]
       id=getroleid(a.href)
       procRole(id,a)
     }
@@ -154,9 +213,19 @@ if (localStorage.wantedskills){
   document.getElementById("wantedskills").value = localStorage.wantedskills
 } else {
   localStorage.wantedskills = "神风诀,心佛,五郎八卦,古谱,九宫剑法,鸳鸯双刀,虬枝,金蛇剑法,玲珑骰,修罗刀"
+  localStorage.ngrolelist = ""
+  localStorage.okrolelist = ""
+}
+if(!localStorage.ngrolelist){
+  localStorage.ngrolelist=""
+}
+if(!localStorage.okrolelist){
+  localStorage.okrolelist=""
 }
 
 addGlobalStyle('.controllbar{position:absolute;right:10%;top:30%;}');
+addGlobalStyle('#ngrolename{background:gray;}');
+addGlobalStyle('#okrolename{background:green;}');
 
 document.getElementById('btshow_all').addEventListener('click',function(){runview();},false);
 document.getElementById('btupdate').addEventListener('click',function(){updateskills();},false);
