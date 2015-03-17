83b238be666deddb8e07b099ea0fe50f3f8162a8
root <root@localhost.localdomain>
2015-03-16 17:36:29 -0700

    deleted:    code/dytracker/app.js 	renamed:    code/dytracker/app.jq.js -> code/webapps/app.jq.js 	renamed:    code/dytracker/angular.min.js -> code/webapps/hbtracker/angular.min.js 	new file:   code/webapps/hbtracker/app.js 	renamed:    code/dytracker/app.manifest -> code/webapps/hbtracker/app.manifest 	renamed:    code/dytracker/bootstrap-theme.min.css -> code/webapps/hbtracker/bootstrap-theme.min.css 	renamed:    code/dytracker/bootstrap.min.css -> code/webapps/hbtracker/bootstrap.min.css 	renamed:    code/dytracker/bootstrap.min.js -> code/webapps/hbtracker/bootstrap.min.js 	renamed:    code/dytracker/htcgi/savelog.py -> code/webapps/hbtracker/htcgi/savelog.py 	renamed:    code/dytracker/index.html -> code/webapps/hbtracker/index.html 	renamed:    code/dytracker/jquery.min.js -> code/webapps/hbtracker/jquery.min.js 	renamed:    code/dytracker/main.py -> code/webapps/hbtracker/main.py 	renamed:    code/dytracker/upcache.sh -> code/webapps/hbtracker/upcache.sh 	renamed:    code/dytracker/index.jq.html -> code/webapps/index.jq.html 	new file:   code/webapps/nosomking.html

---
+++ code/webapps/hbtracker/app.js
@@ -0,0 +1,110 @@
+var app = angular.module("hbRecorder", []);
+
+//common lib
+
+app.controller("hbCtrlMain", function($scope,$http) {
+  //init
+  $scope.sumlog=""
+  $scope.ChkSleep =  function (){
+    var goodcount = 0;
+    //to reverse sleep weight
+    this.chk = function (){
+      n = new Date();
+      k = n.getHours();
+      $scope.hblog += "TotalSmoked,"+$scope.hbs[0].count+","+n.toDateString()+"\n";
+      $scope.hbs[0].count = 0; //new day,reset smoking count
+      if(k <= 23 && k>12){
+        goodcount+=1;
+        $scope.hblog += "TotalGoodSleep,"+goodcount+","+n.toDateString()+"\n";
+        return -1;
+      }
+      return 1;
+    }
+  }
+
+
+  $scope.hbs =  [
+    {"name":"smoking","count":0,"weight":-10,"goal":10},
+    {"name":"mt","count":0,"weight":-50,"goal":1},
+    {"name":"badthing-10","count":0,"weight":-10,"goal":0},
+    {"name":"test","count":0,"weight":0,"goal":10},
+    {"name":"gym","count":0,"weight":50,"goal":3},
+    {"name":"goodthing+10","count":0,"weight":10,"goal":1},
+    {"name":"sleep","count":0,"weight":-50,"goal":1,"fchk":new $scope.ChkSleep()},
+  ];
+
+  $scope.accperiod = 6;
+  $scope.startdate = new Date();
+  $scope.nowdate = $scope.startdate;
+
+
+  if(!localStorage.startmoney){
+        localStorage.startmoney=200;
+  }
+  if(!localStorage.targetmoney){
+    localStorage.targetmoney=parseInt(localStorage.startmoney)+300;
+  }
+  if(!localStorage.curmoney | localStorage.curmoney<=0){
+    localStorage.curmoney=localStorage.startmoney
+  }
+
+  function readmoney(){
+    $scope.curmoney = localStorage.curmoney;
+    $scope.targetmoney = localStorage.targetmoney;
+    $scope.startmoney = localStorage.startmoney;
+  }
+  readmoney();
+
+  //End init
+
+
+  $scope.settheme = function (t){
+    if((t.goal-t.count)*t.weight >0){
+      return "btn-danger"; 
+    }
+    return "btn-success";
+  }
+
+  $scope.hblog = "";
+  $scope.dohblog = function (c,t){
+    $scope.hblog += t.name+","+Date()+","+c+","+$scope.curmoney+"\n";
+  }
+
+  $scope.calmoney = function (c,t){
+    w=t.weight;;
+    if(t.fchk && c>0){ c*=t.fchk.chk();}
+    if(t.goal >= t.count){w=0;}
+    $scope.curmoney = parseInt($scope.curmoney)+c*w;
+    localStorage.curmoney = $scope.curmoney;
+    $scope.dohblog(c,t);
+  }
+
+
+  $scope.recordHB = function (t,c){
+    t.count+=c;
+    if(t.count <= 0){ t.count = 0;}
+    t.theme = $scope.settheme(t);
+    $scope.calmoney(c,t);
+  }
+
+  $scope.resetHB = function (t) {
+    t.count = 0;
+  }
+
+
+  $scope.savelog = function (){
+    $http.post('/htcgi/savelog.py', $scope.hblog).success(function (){
+      $scope.hblog = "Log Saved\n";
+    });
+  }
+  $scope.resetAll = function () {
+    r=confirm("ResetALL? Are you sure?");
+    if(r){
+      localStorage.clear();
+      $http.post('/htcgi/savelog.py', $scope.hblog).success(function (){
+        location.reload();
+      });
+    }
+  }
+
+}); //hbCtrlMain
