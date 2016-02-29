ac09f4a8b9d18ed69738b55925a438f52c93dcf3
DKLuffy <dkluffy@ACA801D4.ipt.aol.com>
2016-03-01 01:00:13 +0800

    modified:   code/stock/tearflow.py 	modified:   othercode/vim/.vimrc.local

--- code/stock/tearflow.py
+++ code/stock/tearflow.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#http://api.money.126.net/data/feed/1002092,money.api
+
+tdlist = {
+    "week":["下跌趋势","上升趋势"],
+    "day":["低开","高开"],
+    "now":["低走","高走"],}
+
+
+def getTrend(oldp,newp,cyc):
+    a = tdlist[cyc]
+
+    if oldp < newp:
+        return tdlist[cyc][1]
+    return tdlist[cyc][0]
+
+
+
