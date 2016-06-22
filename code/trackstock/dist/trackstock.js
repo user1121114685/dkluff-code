46b8be80c19906ec597ac0541bc3c52bce3c4330
root <root@localhost.localdomain>
2016-06-22 08:58:20 -0700

    new file:   code/trackstock/node_modules/angular/LICENSE.md

---
+++ code/trackstock/dist/trackstock.js
@@ -0,0 +1,12 @@
+/*! trackstock - v0.1.0 - 2016-06-22
+* https://github.com/dkluffy/dkluff-code
+* Copyright (c) 2016 dk; Licensed MIT */
+(function(exports) {
+
+  'use strict';
+
+  exports.awesome = function() {
+    return 'awesome';
+  };
+
+}(typeof exports === 'object' && exports || this));
---
+++ code/trackstock/lib/trackstock.js
@@ -0,0 +1,17 @@
+/*
+ * trackstock
+ * https://github.com/dkluffy/dkluff-code
+ *
+ * Copyright (c) 2016 dk
+ * Licensed under the MIT license.
+ */
+
+(function(exports) {
+
+  'use strict';
+
+  exports.awesome = function() {
+    return 'awesome';
+  };
+
+}(typeof exports === 'object' && exports || this));
