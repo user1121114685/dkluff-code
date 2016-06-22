ca7430a067e407fb5b94db4693ed2960435f4916
root <root@localhost.localdomain>
2016-06-22 14:22:41 -0700

    modified:   .gitignore       new file:   code/webapps/trackstock/lib/.jshintrc       new file:   code/webapps/trackstock/lib/angular.min.js       new file:   code/webapps/trackstock/lib/css/bootstrap-theme.css       new file:   code/webapps/trackstock/lib/css/bootstrap-theme.css.map       new file:   code/webapps/trackstock/lib/css/bootstrap-theme.min.css       new file:   code/webapps/trackstock/lib/css/bootstrap-theme.min.css.map       new file:   code/webapps/trackstock/lib/css/bootstrap.css       new file:   code/webapps/trackstock/lib/css/bootstrap.css.map       new file:   code/webapps/trackstock/lib/css/bootstrap.min.css       new file:   code/webapps/trackstock/lib/css/bootstrap.min.css.map       new file:   code/webapps/trackstock/lib/fonts/glyphicons-halflings-regular.eot       new file:   code/webapps/trackstock/lib/fonts/glyphicons-halflings-regular.svg       new file:   code/webapps/trackstock/lib/fonts/glyphicons-halflings-regular.ttf       new file:   code/webapps/trackstock/lib/fonts/glyphicons-halflings-regular.woff       new file:   code/webapps/trackstock/lib/fonts/glyphicons-halflings-regular.woff2       new file:   code/webapps/trackstock/lib/js/bootstrap.js       new file:   code/webapps/trackstock/lib/js/bootstrap.min.js       new file:   code/webapps/trackstock/lib/js/npm.js       new file:   code/webapps/trackstock/lib/trackstock.js

---
+++ code/webapps/trackstock/lib/trackstock.js
@@ -0,0 +1,19 @@
+/*
+ * trackstock
+ * https://github.com/dkluffy/dkluff-code
+ *
+ * Copyright (c) 2016 dk
+ * Licensed under the MIT license.
+ */
+
+(function(exports) {
+
+  'use strict';
+
+  exports.awesome = function() {
+    return 'awesome';
+  };
+
+}(typeof exports === 'object' && exports || this));
+
+require('g2');
