238a29c82545420cf7769871400ce38c85aaffae
DKLuffy <dkluffy@ACA80137.ipt.aol.com>
2017-09-15 23:37:08 +0800

    new file:   note/linux/syslinux_usb.sh

---
+++ note/linux/syslinux_usb.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+dst=/dev/sdd1
+dst_mount=/mnt/usb
+newiso=.
+sudo syslinux -i $dst
+sudo dd if=/usr/lib/syslinux/mbr/mbr.bin of=/dev/sdf conv=notrunc bs=440 count=1
+sudo mount $dst $dst_mount
+#sudo cp /usr/lib/syslinux/menu.c32 $dst_mount/▫
+#sudo cp /boot/memtest86+.bin $dst_mount/memtest▫
+mkdir -p $dst_mount/syslinux
+sudo cp $newiso/isolinux/isolinux.cfg $dst_mount/syslinux/syslinux.cfg
+sudo cp $newiso/isolinux/*.cfg $dst_mount/syslinux/.
+sudo cp $newiso/isolinux/*.cfg $dst_mount/.
+sudo cp $newiso/isolinux/*.c32 $dst_mount/syslinux/.
+sudo cp $newiso/isolinux/*.c32 $dst_mount/.
+#sudo cp /usr/share/misc/pci.ids $dst_mount/▫
+sudo rsync -rv $newiso/live $dst_mount/
